e4012ae1f5bb4273224da11eaa9946aaa325ff49
destinybbrianna <destinybbrianna@gmail.com>
2018-08-06 18:21:04 +0000

    automatically backed up by learn

--- prac.rb
+++ prac.rb
@@ -1,7 +1,7 @@
 first_name = "Destiny"
 last_name = "Ortiz"
 puts first_name 
-puts  "my name is" + "" first_name + " " + last_name
+puts  "my name is" + " " first_name + " " + last_name
 # string interpolation
 puts "My first name is #{first_name} and my last name is #{last_name}"
 age = 17
