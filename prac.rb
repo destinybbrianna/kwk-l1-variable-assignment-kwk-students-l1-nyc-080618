cbd9c8947887615c960a15893a30a1440197b470
destinybbrianna <destinybbrianna@gmail.com>
2018-08-06 18:23:09 +0000

    automatically backed up by learn

--- prac.rb
+++ prac.rb
@@ -5,4 +5,5 @@ puts  "my name is" " " + first_name + " " + last_name
 # string interpolation
 puts "My first name is #{first_name} and my last name is #{last_name}"
 age = 17
+#string interpolation
 puts "my first name"#{first_name} and i am #{age.to_s}
