c7aacd972cc7b69fdfb946b5edb2c2d2c959c58c
destinybbrianna <destinybbrianna@gmail.com>
2018-08-06 18:18:54 +0000

    automatically backed up by learn

--- prac.rb
+++ prac.rb
@@ -3,3 +3,4 @@ last_name = "Ortiz"
 puts first_name 
 puts  "my name is" + first_name + "" + last_name
 # string interpolation
+puts "My first name is #{first_name} and my last name is #{last_name}"
